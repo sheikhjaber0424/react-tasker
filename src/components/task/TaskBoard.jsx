1d4b9fe2c5d0deddef1d592ce4ced812259c36cc
sheikhjaber0424 <sheikhjaber190@gmail.com>
2025-01-02 17:58:01 +0600

    TaskActions component added

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,56 +1,16 @@
+import SearchTask from "./SearchTask";
+import TaskActions from "./TaskActions";
 export default function TaskBoard() {
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
         <div className="p-2 flex justify-end">
-          <form>
-            <div className="flex">
-              <div className="relative overflow-hidden rounded-lg text-gray-50 md:min-w-[380px] lg:min-w-[440px]">
-                <input
-                  type="search"
-                  id="search-dropdown"
-                  className="z-20 block w-full bg-gray-800 px-4 py-2 pr-10 focus:outline-none"
-                  placeholder="Search Task"
-                  required
-                />
-                <button
-                  type="submit"
-                  className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-4"
-                >
-                  <svg
-                    className="h-4 w-4"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                  <span className="sr-only">Search</span>
-                </button>
-              </div>
-            </div>
-          </form>
+          <SearchTask></SearchTask>
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <div className="mb-14 items-center justify-between sm:flex">
-            <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
-            <div className="flex items-center space-x-5">
-              <button className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
-                Add Task
-              </button>
-              <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
-                Delete All
-              </button>
-            </div>
-          </div>
+          <TaskActions></TaskActions>
+
           <div className="overflow-auto">
             <table className="table-fixed overflow-auto xl:w-full">
               <thead>
