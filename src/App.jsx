284504eb0ec0212902f351f4002719a2258671af
sheikhjaber0424 <sheikhjaber190@gmail.com>
2025-01-02 17:18:06 +0600

    Hero component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Hero></Hero>
       <Footer />
     </>
   );
