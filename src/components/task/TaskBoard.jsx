d41d99b56806870023019670c52cbb22777ba57b
sheikhjaber0424 <sheikhjaber190@gmail.com>
2025-01-03 14:54:43 +0600

    TaskBoard values dynamic

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn react",
+    description: "I want to learn React.js",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavourite: false,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -11,7 +21,7 @@ export default function TaskBoard() {
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions></TaskActions>
-          <TaskList></TaskList>
+          <TaskList tasks={tasks}></TaskList>
         </div>
       </div>
     </section>
